5546a5951dc56bce17b593b476792c18199164f3
brnyns <ynsberiane@gmail.com>
2025-04-10 20:57:17 +0200

    httt

--- www.ionos.com/_next/static/chunks/756-1744276638899.1cf4ae4d2649f8b7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[756],{20756:(e,s,n)=>{n.r(s),n.d(s,{businessNameGenerator:()=>a});var t=n(74848),r=n(31330);let a=(0,n(37531).n)(()=>{let[e,s]=(0,r.Ub)();return(0,t.jsx)("div",{children:(0,t.jsx)("iframe",{src:"https://ionos.".concat(s,"/businessnamegenerator/?lang=").concat(e),width:"100%",height:"700px"})})})}}]);
